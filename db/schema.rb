8db265d10d912be8875fdac561876a29cb96891d
Alex Psitos <apsitos08@gmail.com>
2022-08-01 16:11:23 -0400

    Feat: create customers migration and table in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_01_200610) do
+ActiveRecord::Schema.define(version: 2022_08_01_200957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "teas", force: :cascade do |t|
     t.string "title"
