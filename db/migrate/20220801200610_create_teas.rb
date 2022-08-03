21ae8c26c8c15964a31681df019792f827ac8746
Alex Psitos <apsitos08@gmail.com>
2022-08-03 14:37:30 -0400

    Refactor: rewrite migration for Tea table to have integer datatype for brewtime instead of time unit

--- db/migrate/20220801200610_create_teas.rb
+++ db/migrate/20220801200610_create_teas.rb
@@ -4,7 +4,7 @@ class CreateTeas < ActiveRecord::Migration[5.2]
       t.string :title
       t.string :description
       t.integer :temp
-      t.time :brewtime
+      t.integer :brewtime
 
       t.timestamps
     end
