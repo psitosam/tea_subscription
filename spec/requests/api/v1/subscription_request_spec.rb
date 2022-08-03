4a55a75c3f32bdfd47c7ae8ede701372a5e5e3a8
Alex Psitos <apsitos08@gmail.com>
2022-08-03 14:30:32 -0400

    Test: sad path for index action test written and passed

--- spec/requests/api/v1/subscription_request_spec.rb
+++ spec/requests/api/v1/subscription_request_spec.rb
@@ -101,53 +101,75 @@ RSpec.describe 'subscriptions requests' do
     expect(reply[:data]).to have_key :message
     expect(reply[:data][:message]).to eq("Invalid Request")
   end 
-  
-  it 'happy path for index action returns all subscriptions regardless of status' do 
-    customer = create :customer
-    tea_1 = create(:tea, brewtime: "00:11:00") 
-    tea_2 = create(:tea, brewtime: "00:12:00") 
-    tea_3 = create(:tea, brewtime: "00:13:00") 
-    # active subscription 1
-    subscription_1 = customer.subscriptions.create!(title: "#{tea_1.title} Subscription for #{customer.first_name}", price: 12.5, status: 0, frequency: 1)
-    tea_sub_1 = TeaSub.create!(tea_id: tea_1.id, subscription_id: subscription_1.id)
-    # inactive subscription 2 
-    subscription_2 = customer.subscriptions.create!(title: "#{tea_2.title} Subscription for #{customer.first_name}", price: 12.5, status: 1, frequency: 1)
-    tea_sub_2 = TeaSub.create!(tea_id: tea_2.id, subscription_id: subscription_2.id)
-    # active subscription 3
-    subscription_3 = customer.subscriptions.create!(title: "#{tea_3.title} Subscription for #{customer.first_name}", price: 12.5, status: 0, frequency: 1)
-    tea_sub_3 = TeaSub.create!(tea_id: tea_3.id, subscription_id: subscription_3.id)
 
-    payload = {
-      email: customer.email
-    }
-    headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
-
-    get '/api/v1/subscriptions', headers: headers, params: payload
-
-    reply = JSON.parse(response.body, symbolize_names: true)
-
-    expect(reply).to have_key :data 
-    expect(reply[:data]).to be_a Hash 
-    expect(reply[:data]).to have_key :id 
-    expect(reply[:data]).to have_key :type
-    expect(reply[:data]).to have_key :attributes
-    expect(reply[:data][:type]).to eq("customer")
-    expect(reply[:data][:attributes]).to be_a Hash
-    expect(reply[:data][:attributes]).to have_key :first_name
-    expect(reply[:data][:attributes]).to have_key :last_name
-    expect(reply[:data][:attributes]).to have_key :email
-    expect(reply[:data][:attributes]).to have_key :address
-    expect(reply[:data][:attributes]).to have_key :subscriptions
-    expect(reply[:data][:attributes][:subscriptions]).to be_a Array
-    reply[:data][:attributes][:subscriptions].each do |sub|
-      expect(sub).to have_key :id
-      expect(sub).to have_key :title
-      expect(sub).to have_key :price
-      expect(sub).to have_key :status
-      expect(sub).to have_key :frequency
-      expect(sub).to have_key :customer_id
-      expect(sub).to have_key :created_at
-      expect(sub).to have_key :updated_at
+  describe 'the index endpoint' do 
+    before :each do 
+      @customer = create :customer
+      @tea_1 = create(:tea, brewtime: "00:11:00") 
+      @tea_2 = create(:tea, brewtime: "00:12:00") 
+      @tea_3 = create(:tea, brewtime: "00:13:00") 
+      # active subscription 1
+      @subscription_1 = @customer.subscriptions.create!(title: "#{@tea_1.title} Subscription for {@customer.first_name}", price: 12.5, status: 0, frequency: 1)
+      @tea_sub_1 = TeaSub.create!(tea_id: @tea_1.id, subscription_id: @subscription_1.id)
+      # inactive subscription 2 
+      @subscription_2 = @customer.subscriptions.create!(title: "#{@tea_2.title} Subscription for {@customer.first_name}", price: 12.5, status: 1, frequency: 1)
+      @tea_sub_2 = TeaSub.create!(tea_id: @tea_2.id, subscription_id: @subscription_2.id)
+      # active subscription 3
+      @subscription_3 = @customer.subscriptions.create!(title: "#{@tea_3.title} Subscription for {@customer.first_name}", price: 12.5, status: 0, frequency: 1)
+      tea_sub_3 = TeaSub.create!(tea_id: @tea_3.id, subscription_id: @subscription_3.id)
     end 
-  end 
+    
+    it 'happy path for index action returns all subscriptions regardless of status' do 
+
+      payload = {
+        email: @customer.email
+      }
+      headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+
+      get '/api/v1/subscriptions', headers: headers, params: payload
+
+      reply = JSON.parse(response.body, symbolize_names: true)
+
+      expect(reply).to have_key :data 
+      expect(reply[:data]).to be_a Hash 
+      expect(reply[:data]).to have_key :id 
+      expect(reply[:data]).to have_key :type
+      expect(reply[:data]).to have_key :attributes
+      expect(reply[:data][:type]).to eq("customer")
+      expect(reply[:data][:attributes]).to be_a Hash
+      expect(reply[:data][:attributes]).to have_key :first_name
+      expect(reply[:data][:attributes]).to have_key :last_name
+      expect(reply[:data][:attributes]).to have_key :email
+      expect(reply[:data][:attributes]).to have_key :address
+      expect(reply[:data][:attributes]).to have_key :subscriptions
+      expect(reply[:data][:attributes][:subscriptions]).to be_a Array
+      reply[:data][:attributes][:subscriptions].each do |sub|
+        expect(sub).to have_key :id
+        expect(sub).to have_key :title
+        expect(sub).to have_key :price
+        expect(sub).to have_key :status
+        expect(sub).to have_key :frequency
+        expect(sub).to have_key :customer_id
+        expect(sub).to have_key :created_at
+        expect(sub).to have_key :updated_at
+      end 
+    end
+
+    it 'sad path returns error message and 404' do 
+
+      payload = {
+        email: @customer.first_name
+      }
+      headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+
+      get '/api/v1/subscriptions', headers: headers, params: payload
+
+      reply = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(reply).to have_key :data
+      expect(reply[:data]).to be_a Hash
+      expect(reply[:data]).to have_key :message
+      expect(reply[:data][:message]).to eq("Invalid Request")
+    end 
+  end  
 end
